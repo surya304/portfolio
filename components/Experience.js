78434e181d8541467202655d7d33ff603e969c6b
Surya <ksaisurya304@gmail.com>
2024-03-07 12:40:29 -0500

    commit data

--- components/Experience.js
+++ components/Experience.js
@@ -141,7 +141,7 @@ export function Experience() {
           {videformData.map((data, index) => (
 
             <button
-              key={index}
+              key={data}
               type="button"
               className="relative m-1 inline-flex items-center rounded-full px-3 py-2 text-sm font-semiboldring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10 mr-2" style={{ 'color': 'rgb(94 234 212 / 100%)', 'backgroundColor': 'hsla(160, 52%, 71%, .1)'}}
               
@@ -187,7 +187,7 @@ export function Experience() {
           {greyMetricsData.map((data, index) => (
 
             <button
-              key={index}
+              key={data}
               type="button"
               className="relative m-1 inline-flex items-center rounded-full px-3 py-2 text-sm font-semiboldring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10 mr-2" style={{ 'color': 'rgb(94 234 212 / 100%)', 'backgroundColor': 'hsla(160, 52%, 71%, .1)'}}
               
@@ -243,11 +243,11 @@ export function Experience() {
           {aquoDigitalData.map((data, index) => (
               
               <button
-                key={index}
+                key={data}
                 type="button"
                 className="relative m-1 inline-flex items-center rounded-full px-3 py-2 text-sm font-semiboldring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10 mr-2" style={{ 'color': 'rgb(94 234 212 / 100%)', 'backgroundColor': 'hsla(160, 52%, 71%, .1)'}}
-                
               >
+                
                 {data}
               </button>
   
