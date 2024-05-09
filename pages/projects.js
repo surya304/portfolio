a0d35a3ebc343088f3a589b35a28ed3d75380e26
Surya <ksaisurya304@gmail.com>
2024-05-09 18:40:17 -0400

    changed url

--- pages/projects.js
+++ pages/projects.js
@@ -25,7 +25,7 @@ const Projects = () => {
       description: 'Created an App Where You Can put all your social media Links In one place and share it you can keep it in insta bio or any where you want to share your social media links.',
       imageUrl: 'https://mybucket4345.s3.us-east-2.amazonaws.com/portfolio/tapshort.png',
       redirectURL: 'https://github.com/surya304/BioTree',
-      websiteurl: 'https://tapshort.com',
+      websiteurl: 'https://tapshort.onrender.com',
       showbtn: true
 
     }, 
