461f780276a1689fd72e0a56f1c8187102a4c237
Surya <ksaisurya304@gmail.com>
2024-09-05 20:41:45 -0400

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,27 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 function MyDocument() {
   return (
     <Html>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Montserrat:wght@400;600;700&family=Poppins:wght@400;600;700&family=Raleway:wght@400;600;700&family=Ubuntu:wght@400;500;700&family=Shrikhand&display=swap"
-        rel="stylesheet"/>
-       
+        <link
+          href="https://fonts.googleapis.com/css2?family=Pacifico&family=Montserrat:wght@400;600;700&family=Poppins:wght@400;600;700&family=Raleway:wght@400;600;700&family=Ubuntu:wght@400;500;700&family=Shrikhand&display=swap"
+          rel="stylesheet"
+        />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7PKEFX1G83"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7PKEFX1G83');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
