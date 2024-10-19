fb7f12a476418c81a974c9fbb9c52e9dd1105015
Surya <ksaisurya304@gmail.com>
2024-10-18 21:03:34 -0400

    updated styles 1

--- components/Experience.js
+++ components/Experience.js
@@ -181,8 +181,8 @@ export function Experience() {
               <button
                 key={data}
                 type="button"
-                className="relative m-1 inline-flex items-center rounded-full px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10 mr-2"
-                style={{ 'color': 'rgb(94 234 212 / 100%)', 'backgroundColor': 'hsla(160, 52%, 71%, .1)' }}
+                className="relative m-1 inline-flex items-center rounded-full px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10 mr-2 "
+                style={{ 'color': 'rgb(94 234 212 / 100%)', 'backgroundColor': 'hsla(160, 52%, 71%, .1)'  }}
               >
                 {data}
               </button>
